891cd2d70e8262fdcadb8c33322b1fb7a2f93921
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2019-11-06 20:39:50 +0300

    Bubble-sort (Пузырьковая сортировка)

---
+++ MyProgramm/BubbleSort.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BubbleSort {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
+
+        int arr[] = {a, b, c, d, e};
+        bubbleSort(arr);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = arr.length-1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int a = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = a;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(arr[i]);
+    }
+}
