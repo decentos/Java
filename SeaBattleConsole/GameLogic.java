f081eb883105b492656c841d1e22b394d6ad14e1
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2019-11-19 18:55:34 +0300

    [+] Create game logic

---
+++ SeaBattleConsole/GameLogic.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class GameLogic {
+
+        private ArrayList<String> locationCells;
+        private String name;
+
+        public void setLocationCells(ArrayList<String> loc) {
+                locationCells = loc;
+        }
+
+        public void setName(String n) {
+                name = n;
+        }
+
+        String checkYourself(String userInput) {
+
+                String result = "Мимо";
+                int index = locationCells.indexOf(userInput);
+
+                if (index >= 0) {
+                        locationCells.remove(index);
+
+                        if (locationCells.isEmpty()) {
+                                result = "Потопил";
+                                System.out.println("Ой! Вы потопили " + name + " :( ");
+                        }
+                        else {
+                                result = "Попал";
+                        }
+                }
+
+                return result;
+        }
+}
